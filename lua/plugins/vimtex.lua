a1aaa3ece5e252f46e269bf857ea2487483eb466
pirosapphic <rocchietti.march.paolo@gmail.com>
2026-03-14 23:07:29 +0100

    some vimtex tweaking

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,6 +1,10 @@
 vim.g.vimtex_compiler_latexmk = {
     aux_dir = "aux", -- create a directory called aux that will contain all the auxiliary files
 }
+vim.g.vimtex_quickfix_method = 'pplatex'
+vim.g.vimtex_quickfix_open_on_warning = 0
+vim.g.vimtex_quickfix_autoclose_after_keystrokes = 3
+
 return
 {
   "lervag/vimtex",
