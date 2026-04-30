af36fa23f05b3071f3b87569237833f8da412ed6
pirosapphic <rocchietti.march.paolo@gmail.com>
2026-04-30 18:19:09 +0200

    added yazi

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,4 +1,4 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-vim.keymap.set("n","<leader>cd", vim.cmd.Ex) --normal mode, combo, bind
+--vim.keymap.set("n","<leader>cd", vim.cmd.Ex) --normal mode, combo, bind
 vim.keymap.set("","<leader>yy", '"+y')
